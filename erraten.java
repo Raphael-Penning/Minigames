61cdf4eb31f0329f3ebebb28f40ea84aa4cf67bb
BTWgamer201 <74791929+BTWgamer201@users.noreply.github.com>
2021-04-13 10:42:15 +0200

    Update erraten.java

--- erraten.java
+++ erraten.java
@@ -1,5 +1,28 @@
-package main;
+
+import java.util.Random;
+import java.util.Scanner
 
 public class {
-  //wird geändert
+
+private int maxnumber = 10; 
+
+  public static void main(String[] args)
+  {
+    erstelleZufallsZahl();
+  }
+  
+  
+  public static void erstelleZufallsZahl() {
+    Random zufallsgenerator = new Random();
+    for (int i = 0; i < 1; i++) {
+      int zahl = zufallsgenerator.nextInt(maxnumber);
+      System.out.println("Gebe eine Zahl zwischen 0 und " + maxnumber + " ein!");
+      Scanner scan = new Scanner(System.in);
+      int number = scan.nextInt();
+        if(zahl == number) {
+            System.out.println("Richtig erraten die Zahl war: " + zahl + " !");
+        }else System.out.println("Die Zahl war " + zahl + "! NEXT TRY");
+    }
+  }
 }
+
